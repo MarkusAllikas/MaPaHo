96a513b4474dba7f0edb9c25914f9b704f00fa81
MarkusAllikas <markusallikas03@gmail.com>
2021-04-26 12:35:07 +0300

    Arvauspeli on valmis.

--- Projekti 2 [Arvauspeli]/arvauspeli.js
+++ Projekti 2 [Arvauspeli]/arvauspeli.js
@@ -1,4 +1,6 @@
-function Arvaus() {
+
+// Patrikin kirjoittama koodi
+/*function Arvaus() {
     guess = document.getElementById("guess").value;
     guesses = document.getElementById("tulos");
 
@@ -17,4 +19,71 @@ function eraseText() {
 }
 
 
-var sattunainenLuku = Math.floor(Math.random()*100);
+var sattunainenLuku = Math.floor(Math.random()*100);*/
+// Patrikin kirjoittama koodi loppuu tähän.
+//tämä ei toiminut mut en halunnut poistaakaan toisen työtä -Markus
+
+
+var sattunainenLuku = Math.floor(Math.random()*10)+1; // luo satunnaisen luvun 1-10
+var arvausKerrat = 0;
+
+window.onload= function alusta() //tekee napit 1-10
+{
+  var napit10 = '';
+
+  for(var i = 1; i <=10; i++)
+  {
+    for(var j = 0; j < 3; j++)
+    {
+      napit10 += '<button class= "napit" type="button" name="' + i +'" onclick="arvaa(this)" value = '+i+'>'+i+'</button>';
+      if(i < 10 && j != 2)
+      {
+        i++;
+      }
+      else
+      {
+        j=3;
+      }
+
+    }
+    napit10+= '<br>';
+  }
+  document.getElementById('napit').innerHTML = napit10;
+
+}
+
+/*Tarkistaa onko käyttäjän arvaama luku oikea,
+liian pieni vai liian iso ja tarkistaa myös sen,
+että kuinka monessa kerrassa luku arvattiin, jos se arvattiin.*/
+function arvaa(arpa)
+{
+  arvausKerrat++;
+  var arvaus = arpa.value;
+  if(arvausKerrat <= 3)
+  {
+    if(arvaus < sattunainenLuku)
+    {
+      alert("arvasit liian matalan!");
+      var x = '';
+      for(var luvut = 1; luvut < arvaus; luvut++)
+      {
+        x = document.getElementsByName('' + luvut + '');
+        console.log(x);
+        x.style.nayta = "none";
+      }
+    }
+    else if(arvaus > sattunainenLuku)
+    {
+      alert("arvasit liian korkean!");
+    }
+    else if (arvaus == sattunainenLuku)
+    {
+      alert("Sinä arvasit oikein, käytit " + arvausKerrat + " kertaa!");
+    }
+    else
+    {
+      alert('Käytit kaikki kertasi');
+    }
+  }
+
+}
