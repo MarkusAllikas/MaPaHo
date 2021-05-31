7e06b2217d83a06acfbfe2ca53f5eb5e18ec2598
MarkusAllikas <markusallikas03@gmail.com>
2021-05-31 21:18:34 +0300

    kesken

--- Oma projekti [Piirrustuspeli]/OmaProj.js
+++ Oma projekti [Piirrustuspeli]/OmaProj.js
@@ -1,3 +1,8 @@
 
-var canvas = document.getElementById('piirrustusAla');
-var ctx = canvas.getContext('2d');
+const canvas = document.getElementById('piirra');
+canvas.width = window.innerwidth - 60
+canvas.height = 400;
+
+let context = canvas.getContext("2d");
+context.fillStyle = "white";
+context.fillRect(0, 0, canvas.width, canvas.height);
